02f8a5975e111c7e48fc10e18d1644188492764e
Shane Honanie <shaneh425@gmail.com>
2019-11-19 19:39:35 -0800

    Virtuals & Casecade Delete Courses

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
 	// Finding resource
-	query = Bootcamp.find(JSON.parse(queryStr));
+	query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
 	// Select Fields
 	if (req.query.select) {
@@ -43,7 +43,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
 	// Pagination
 	const page = parseInt(req.query.page, 10) || 1;
-	const limit = parseInt(req.query.limit, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 10;
 	const startIndex = (page - 1) * limit;
 	const endIndex = page * limit;
 	const total = await Bootcamp.countDocuments();
@@ -125,12 +125,14 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route     DELETE /api/v1/bootcamps/:id
 // @access    Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-	const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+	const bootcamp = await Bootcamp.findById(req.params.id);
 
 	if (!bootcamp)
 		return next(
 			new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
 		);
+
+	bootcamp.remove();
 
 	res.status(200).json({ success: true, data: {} });
 });
